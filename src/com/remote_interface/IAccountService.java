21fabd11d49d22b6ee4be07282c91045aa7df3e1
TerenceZH <1033281892@qq.com>
2014-12-17 15:43:09 +0800

    IAccountService

--- src/com/remote_interface/IAccountService.java
+++ src/com/remote_interface/IAccountService.java
@@ -7,7 +7,11 @@ import java.util.ArrayList;
 import com.model.Account;
 
 public interface IAccountService extends Remote{
-
+	/**
+	 * Ôö ¸Ä ²é
+	 * @param a
+	 * @throws RemoteException
+	 */
 	public void addAccount(Account a)throws RemoteException;
 	public void modAccount(Account a)throws RemoteException;
 	public ArrayList<Account> queryAccount(String name)throws RemoteException;
