184252bbafd0ede90409ad1ae838370e49c488b6
TerenceZH <1033281892@qq.com>
2014-12-19 11:34:43 +0800

    import

--- src/com/remote_interface/IImportService.java
+++ src/com/remote_interface/IImportService.java
@@ -4,10 +4,41 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
+import com.model.Commodity;
 import com.model.ImportBill;
+import com.model.PortBillItem;
 
 public interface IImportService extends Remote{
-	public void addImportBill(ImportBill bill)throws RemoteException;
-	public ArrayList<ImportBill> queryImportBill(String time1,String time2)throws RemoteException;
-	public String getNewId() throws RemoteException;
+	/**
+	 * 进货单  编号自动生成  dialog  type=0进货 1退货
+	 * @param customer
+	 * @param warehouse
+	 * @param operator 
+	 * @param desc
+	 * @param time  yyyy-MM-dd HH:mm:ss
+	 * @param total
+	 * @throws RemoteException
+	 */
+	public void addImport(int type,String customer, String warehouse,String operator, ArrayList<String>s,String desc,String time, double total )throws RemoteException;
+	/**
+	 * 获取可以退货的单子
+	 * @param i 按照时间倒叙排序的第i个  start from 1 
+	 * @return 超出个数返回null
+	 * @throws RemoteException
+	 */
+	public ImportBill getCancelableImport(int i)throws RemoteException;
+	/**
+	 * 获取和单子有关的商品列表
+	 * @param no
+	 * @return
+	 * @throws RemoteException
+	 */
+	public ArrayList<PortBillItem>getBillItems(String no)throws RemoteException;
+	/**
+	 * 根据编号获得商品   
+	 * @param no
+	 * @return  不存在null
+	 * @throws RemoteException
+	 */
+	public Commodity getCommodity(String no)throws RemoteException;
 }
